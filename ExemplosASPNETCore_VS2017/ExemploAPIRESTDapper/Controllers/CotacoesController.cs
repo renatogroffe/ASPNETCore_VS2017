1347eee28f5eccd361215a02cf55e1d85fb070a4
Renato Groffe <renato.groffe@yahoo.com.br>
2017-03-15 23:25:38 -0300

    ASP.NET Core no Visual Studio 2017
    Exemplos de uso do ASP.NET Core no Visual Studio 2017

---
+++ ExemplosASPNETCore_VS2017/ExemploAPIRESTDapper/Controllers/CotacoesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploAPIRESTDapper.Controllers
+{
+    [Route("api/[controller]")]
+    public class CotacoesController : Controller
+    {
+        [HttpGet("{id}")]
+        public Cotacao Get(
+            [FromServices]CotacoesDAO cotacoesDAO,
+            string id)
+        {
+            return cotacoesDAO.Obter(id);
+        }
+    }
+}
---
+++ ExemplosASPNETCore_VS2017/ExemploAPIRESTDapper/CotacoesDAO.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+namespace ExemploAPIRESTDapper
+{
+    public class CotacoesDAO
+    {
+        private IConfiguration _configuracoes;
+        public CotacoesDAO(IConfiguration config)
+        {
+            _configuracoes = config;
+        }
+
+        public Cotacao Obter(string idMoeda)
+        {
+            using (SqlConnection conexao = new SqlConnection(
+                _configuracoes.GetConnectionString("BaseCotacoes")))
+            {
+                return conexao.QueryFirstOrDefault<Cotacao>(
+                    "SELECT " +
+                        "Sigla, " +
+                        "NomeMoeda, " +
+                        "UltimaCotacao, " +
+                        "Valor " +
+                    "FROM dbo.Cotacoes " +
+                    "WHERE Sigla = @SiglaMoeda ",
+                    new { SiglaMoeda = idMoeda });
+            }
+        }
+    }
+}
